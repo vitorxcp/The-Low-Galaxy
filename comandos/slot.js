912102eb99b9f7bf575eb08de267dc1151b93798
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2022-02-10 13:06:20 +0000

    Add slot.js

---
+++ comandos/slot.js
@@ -0,0 +1,46 @@
+exports.run = async (bot, message, args) => {
+  if (args[0] === "u") {
+    emojis = [
+      "<a:emoji_34:917181658954862642>",
+      "<a:emoji_34:917181691179724850>",
+      "<a:fire_green:921842388266254468>",
+      "<a:fuegorojo:921845059689791528>"
+    ]
+    mig1 = emojis[Math.floor(Math.random() * emojis.length)]
+    mig2 = emojis[Math.floor(Math.random() * emojis.length)]
+    mig3 = emojis[Math.floor(Math.random() * emojis.length)]
+    const Discord = require("discord.js")
+    let desg = "`|-=-=-=-X-=-=-=-|`"
+    embed = new Discord.MessageEmbed()
+      .setColor("BLACK")
+      .setTitle("Roleta da Sorte:")
+      .setTimestamp()
+      .setDescription("\n" + desg + "\n**-=[ " + mig1 + " | `...` | `...` ]=-**\n" + desg + "\n")
+    let msg = await message.reply(" ", embed)
+    setTimeout(() => {
+      embed = new Discord.MessageEmbed()
+        .setColor("BLACK")
+        .setTitle("Roleta da Sorte:")
+        .setTimestamp()
+        .setDescription("\n" + desg + "\n**-=[ " + mig1 + " | " + mig2 + " | `...` ]=-**\n" + desg + "\n")
+      msg.edit(embed)
+    }, 500)
+    setTimeout(() => {
+      embed = new Discord.MessageEmbed()
+        .setColor("BLACK")
+        .setTimestamp()
+        .setTitle("Roleta da Sorte:")
+        .setDescription("\n" + desg + "\n**-=[ " + mig1 + " | " + mig2 + " | " + mig3 + " ]=-**\n" + desg + "\n")
+      msg.edit(embed)
+    }, 1000)
+  } else {
+    const Discord = require("discord.js")
+    embed = new Discord.MessageEmbed()
+      .setColor("BLUE")
+      .setTimestamp()
+      .setTitle("Roleta da Sorte!")
+      .setDescription("**Nesta Roleta você tem a chance de ganhar muitos *Galaxy's* dependendo da sua sorte ;3**\n\n> Para jogar use *`t.slot u`*! Cada rodada custa *`200`* Galaxy.")
+    message.reply(" ", embed)
+  }
+}
+exports.help = { name: "slot", aliases: [] }
